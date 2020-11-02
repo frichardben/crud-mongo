ce7361c90038aff27b21d860eda9d167e6e27dcb
richard <frichardben@yahoo.com.br>
2020-11-02 15:06:32 -0300

    feat: added repository customer

---
+++ src/main/java/com/policy/policy/repositories/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.policy.policy.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.policy.policy.models.Customer;
+
+public interface CustomerRepository extends MongoRepository<Customer, Long>{
+
+}
