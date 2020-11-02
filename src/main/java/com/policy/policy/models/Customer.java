4fa46dc932e8d03ada9b58840230dc4f662f6f05
richard <frichardben@yahoo.com.br>
2020-11-02 15:01:49 -0300

    feat: added model customer

---
+++ src/main/java/com/policy/policy/models/Customer.java
@@ -0,0 +1,71 @@
+package com.policy.policy.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "customer")
+public class Customer {
+	
+	@Id
+	private long id;
+	
+	private String fullname;
+	
+	private String cpf;
+	
+	private String city;
+	
+	private String uf;
+
+	
+	
+	public Customer() {
+		super();
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	
+	public String getFullname() {
+		return fullname;
+	}
+
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+	
+	
+	
+	
+	
+}
